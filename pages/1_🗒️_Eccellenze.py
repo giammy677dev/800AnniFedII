d2efac3f821d67e9a4503d081793ca87e1dff37f
Mav3rick26 <marcoperillo26@gmail.com>
2023-06-22 13:00:07 +0200

    set_page_config

---
+++ pages/1_🗒️_Eccellenze.py
@@ -0,0 +1,7 @@
+from utils import st
+
+st.set_page_config(
+    page_title="Eccellenze",
+    page_icon="️📊",
+    layout="wide"
+)
---
+++ pages/2_🗒️_Campi_Di_Ricerca.py
@@ -0,0 +1,7 @@
+from utils import st
+
+st.set_page_config(
+    page_title="Campi di Ricerca",
+    page_icon="️📊",
+    layout="wide"
+)
