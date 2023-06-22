a7e6774bf1b59b5fb912d698d76e302386fdaab4
giammy677dev <giammy677@gmail.com>
2023-06-22 13:06:55 +0200

    Campi di Ricerca, inizio

--- pages/2_🗒️_Campi_Di_Ricerca.py
+++ pages/2_🗒️_Campi_Di_Ricerca.py
@@ -1,7 +1,18 @@
-from utils import st
+from utils import st, conn
 
 st.set_page_config(
     page_title="Campi di Ricerca",
     page_icon="️📊",
     layout="wide"
 )
+
+st.title('🌍 Campi di Ricerca')
+
+st.write('''In questa sezione bla bla bla.
+        ''')
+
+# Aggiungiamo il filtro per selezionare la macro-categoria
+query = "MATCH (u:Utente) RETURN u.screen_name ORDER BY u.screen_name"
+query_results = conn.query(query)
+string_results = [record['u.screen_name'] for record in query_results]
+selected_user = st.selectbox('Seleziona l\'utente:', string_results, index=11)
