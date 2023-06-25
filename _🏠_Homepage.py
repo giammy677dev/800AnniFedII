1438ae4ecd372eba5882e7061166b5f592612bfe
Mav3rick26 <marcoperillo26@gmail.com>
2023-06-25 13:12:47 +0200

    Prima bozza homepage

--- _🏠_Homepage.py
+++ _🏠_Homepage.py
@@ -6,4 +6,20 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title('🏠Homepage')
+def elenco_bullet(testo_grassetto, testo_normale):
+    st.markdown(f"- <span style='color:#3e8ad2'><b>{testo_grassetto}</b></span>: {testo_normale}",
+                unsafe_allow_html=True)
+
+# Immagine di copertina
+st.image('LogoUniRemakeWhite.png', use_column_width=True)
+
+st.title("HomePage - 800 anni Federico II")
+st.header("Dashboard di analitiche sui progetti ️")
+st.write("""bla bla blaa""")
+
+st.write("""Le analitiche effettuate che ritroviamo in questa dashboard sono le seguenti:""")
+
+# Elenco con bullet list e link ai collegamenti delle sezioni
+elenco_bullet("Prima Analitica", "Presentazione pagina analitiche")
+elenco_bullet("Seconda Analitica", "Presentazione pagina analitiche")
+elenco_bullet("Terza Analitica", "Presentazione pagina analitiche")
